85298d2e87da48f0d58089b69fe26dddc7e9bdfd
romainrince <romainrince@89631569-0592-016a-fb91-e3cd5ece99d8>
2012-10-02 08:26:18 +0000

    add .project

---
+++ trunk/Middleware/src/client/ClientForum.java
@@ -0,0 +1,10 @@
+package client;
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class ClientForum extends JFrame
+{
+JButton boutonInscriptionSport = new JButton("sport");
+}
