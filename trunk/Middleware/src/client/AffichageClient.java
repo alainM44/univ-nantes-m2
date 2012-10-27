338cec2dccbb9a3b6f0246d4832b734bf58b2213
noircantat@hotmail.fr <noircantat@hotmail.fr@89631569-0592-016a-fb91-e3cd5ece99d8>
2012-10-27 17:51:23 +0000

    Grand ménage suite

--- trunk/Middleware/src/client/AffichageClient.java
+++ trunk/Middleware/src/client/AffichageClient.java
@@ -16,11 +16,9 @@
 
 package client;
 
-import ihm.Placement;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
@@ -30,8 +28,6 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.KeyStroke;
-
 import serveur.IAffichageClient;
 import serveur.ISujetDiscussion;
 
